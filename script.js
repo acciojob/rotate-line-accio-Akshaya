9142cdeb95db0ba688198de3648865400d4fc322
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-05-09 07:37:17 -0700

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const line = document.getElementById('line');
 
 let angle = 0;
 
-setInterval(){
+setInterval(()=>{
 	angle+=2;
 	line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
 }, 20);
